6580eae30864acb3658ba511a171c69de11dc895
Sameer Sangpal <66773934+SameerSangpal@users.noreply.github.com>
2025-06-05 18:34:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	if(!s){
+		return s;
+	}
+	const spaceindex = s.indexOf(" ");
+	if(spaceindex === -1){
+		return s;
+	}
+	return s.substring(0, spaceindex);
 }
 
 // Do not change the code below
